c2c7ab008b1f86b945e9afc66135a297a70d0fbe
Kisly <gleb.kuleshevskiy@gmail.com>
2024-04-10 02:01:41 +0300

    Added view of translator

--- src/app/App.vue
+++ src/app/App.vue
@@ -5,7 +5,9 @@ import { MainPage } from '@/pages/main-page'
 
 <template>
 	<div class="app-wrapper">
-		<MainPage />
+		<div class="main-page-wrapper">
+			<MainPage />
+		</div>
 	</div>
 </template>
 
@@ -13,6 +15,16 @@ import { MainPage } from '@/pages/main-page'
 .app-wrapper
 {
 	display: flex;
+	width: 100%;
 	height: 100vh;
+	background-color: #121214;
+}
+
+.main-page-wrapper
+{
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+	flex-grow: 1;
 }
 </style>
--- src/pages/main-page/ui/MainPage.vue
+++ src/pages/main-page/ui/MainPage.vue
@@ -1,5 +1,165 @@
 <template>
-	<div>
-		
+	<div class="translator-wrapper">
+		<div class="translator-header">
+			<div class="translator-header__native-language">
+				<button class="translator-header__native-language--select">English</button>
+			</div>
+			<div class="translator-header__arrows">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white"> 
+					<path fill-rule="evenodd" d="M11.8.603a.5.5 0 0 0-.8.4v2H1a1 1 0 1 0 0 2h10v2a.5.5 0 0 0 .8.4l3.997-3a.5.5 0 0 0 0-.8l-3.997-3ZM5 13.003v2a.5.5 0 0 1-.8.4l-3.997-3a.5.5 0 0 1 0-.8l3.997-3a.5.5 0 0 1 .8.4v2h10a1 1 0 0 1 0 2H5Z" clip-rule="evenodd"></path>
+				</svg>
+			</div>
+			<div class="translator-header__foreign-language">
+				<button class="translator-header__foreign-language--select">Sign language</button>
+			</div>
+		</div>
+		<div class="translator-bottom">
+			<div class="translator-bottom__native-language">
+				<div contenteditable="true" class="translator-bottom__native-language--input">
+					daasdas
+				</div>
+			</div>
+			<div class="translator-bottom__translation">
+				<div contenteditable="true" class="translator-bottom__translation--output">
+					sdasdasd
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
+
+<style scoped lang="scss">
+$border-radius: 23px;
+
+.translator-wrapper
+{
+	margin-top: 32px;
+	display: flex;
+	flex-direction: column;
+	background-color: #252427;
+	border-radius: $border-radius;
+}
+
+$font-size: 20px;
+$font-color: #D3D3C9;
+$language-hover: #2E2D2F;
+.translator-header
+{
+	display: grid;
+	grid-template: 56px / 1fr 0.1fr 1fr;
+	align-items: center;
+	justify-items: center;
+	user-select: none;
+
+	&__arrows {
+		cursor: pointer;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 40px;
+		height: 40px;		
+
+		&:hover {
+			background-color: #313134d0;
+			border-radius: 50%;
+		}
+	}
+
+	&__native-language {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		border-top-left-radius: $border-radius;
+		cursor: pointer;
+
+		&:hover {
+			background-color: $language-hover;
+		}
+
+		&--select {
+			background-color: unset;
+			border: none;
+
+			font-size: $font-size;
+			color: $font-color;
+		}
+	}
+
+	&__foreign-language {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		border-top-right-radius: $border-radius;
+		cursor: pointer;
+
+		&:hover {
+			background-color: $language-hover;
+		}
+
+		&--select {
+			background-color: unset;
+			border: none;
+
+			font-size: $font-size;
+			color: $font-color;
+		}
+	}
+}
+
+$input-field-height: 237px;
+$input-field-width: 574px;
+$input-size: 24px;
+$input-color: #FFFFFF;
+$scroll-bar-color: #686868 #424242;
+.translator-bottom
+{
+	display: flex;
+	flex-direction: row;
+
+	&__native-language {
+		padding: 23px 67px 71px 23px;
+		border-top: 1px solid #39393D;
+		width: $input-field-width;
+		height: $input-field-height;
+
+		&--input {
+			padding-right: 5px;
+			outline: none;
+			font-size: $input-size;
+			font-family: sans-serif;
+			color: $input-color;
+
+			height: 100%;
+			width: 100%;
+			overflow-y: auto;
+			scrollbar-color: $scroll-bar-color;
+		}
+	}
+
+	&__translation {
+		padding: 23px 67px 71px 23px;
+		width: $input-field-width;
+		height: $input-field-height;
+		background-color: #39393D;
+		border-radius: 0 0 $border-radius 0;
+
+		&--output {
+			padding-right: 5px;
+			outline: none;
+			font-size: $input-size;
+			font-family: sans-serif;
+			color: $input-color;
+
+			height: 100%;
+			width: 100%;
+			overflow-y: auto;
+			scrollbar-color: $scroll-bar-color;
+		}
+	}
+}
+</style>
