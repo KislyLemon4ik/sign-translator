437bae20fdbe501bc723e13ea94a7fe4ac48162d
Kisly <gleb.kuleshevskiy@gmail.com>
2024-04-11 01:37:01 +0300

    Added sidebar menu

--- src/app/App.vue
+++ src/app/App.vue
@@ -5,9 +5,7 @@ import { MainPage } from '@/pages/main-page'
 
 <template>
 	<div class="app-wrapper">
-		<div class="main-page-wrapper">
-			<MainPage />
-		</div>
+		<MainPage />
 	</div>
 </template>
 
@@ -17,14 +15,5 @@ import { MainPage } from '@/pages/main-page'
 	display: flex;
 	width: 100%;
 	height: 100vh;
-	background-color: #121214;
-}
-
-.main-page-wrapper
-{
-	display: flex;
-	justify-content: center;
-	align-items: flex-start;
-	flex-grow: 1;
 }
 </style>
---
+++ src/widgets/translator/index.ts
@@ -0,0 +1,3 @@
+import Translator from "./ui/Translator.vue"
+
+export { Translator }
